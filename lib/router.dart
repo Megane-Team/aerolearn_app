51ba06d54a3accc19639853eb3bc78b4a5fe2615
hdzz12 <miitodc02@gmail.com>
2024-12-09 14:14:23 +0700

    UI/Feedback: 96%

--- lib/router.dart
+++ lib/router.dart
@@ -10,7 +10,7 @@ import 'package:aerolearn/page/sub_page/feedback.dart';
 import 'package:aerolearn/page/sub_page/materi.dart';
 
 final router = GoRouter(
-  initialLocation: ('/history'),
+  initialLocation: ('/profile'),
   routes: [
     GoRoute(
       path: "/login",
